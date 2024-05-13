429f4d22afcafc8683fa22fe4b0db77b7d8e4a46
Gramet Maximiliano <maxigramet03@gmail.com>
2024-05-13 12:02:43 -0300

    port again

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-const port = import.meta.env.VITE_PORT;
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: port,
-  },
-});
+export default async ({ command, mode }) => {
+  await loadEnv(mode, process.cwd()); // Load environment variables from .env files
+
+  const env = loadEnv(mode, process.cwd()); // Load environment variables again for immediate use
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: env.VITE_PORT || 3000, // Example usage of an environment variable for port
+    },
+  });
+};
