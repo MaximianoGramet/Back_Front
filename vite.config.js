6d074a95eed9f8c5019333ba9912a10777ae359d
Gramet Maximiliano <maxigramet03@gmail.com>
2024-05-13 11:44:24 -0300

    port

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const port = import.meta.env.VITE_PORT;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: import.meta.env.VITE_PORT,
+    port: port,
   },
 });
